0ff6e5da9bde7397ed5b318f2a73369958821aa9
sourceTree <sourcetree@mail.com>
2024-01-04 09:25:53 -0700

    [16] Using lower bound wildcards

--- Hello.java
+++ Hello.java
@@ -6,20 +6,30 @@ import java.util.List;
 
 class Hello{
     public static void main(String[] args) {    
-        List<Integer> num = new ArrayList<>();
-        num.add(1);
-        num.add(2);
-        printList(num);
+        List<Integer> intgrs = new ArrayList<>();
+        intgrs.add(1);
+        intgrs.add(2);
+        printList(intgrs);
 
-        List<Double> dbl = new ArrayList<>();
-        dbl.add(1d);
-        dbl.add(2d);        
-        printList(dbl);
+        List<Double> doubles = new ArrayList<>();
+        doubles.add(1d);
+        doubles.add(2d);        
+        printList(doubles);
+
+        List<Number> numbers = new ArrayList<>();
+        numbers.add(1.5);
+        addToList(numbers, 1.3);
     }
 
-    private static void printList(List<? extends Number> nomor) {     //1. if you specify wildcard ?
+    private static void addToList(List<? super Number> numbers, Number i) {
+        numbers.forEach(System.out::println);        
+        numbers.add(i);
+        //Number cobak = numbers.get(0);    //lowerbound can add but can't get
+    }
+
+    private static void printList(List<? extends Number> nomor) {
         nomor.forEach(System.out::println);
         Number cobak = nomor.get(0);
-        //nomor.add(5);   //tetep gak bisa add
+        //nomor.add(5);   //upperbound can't add but can get
     }    
 }
