5ae0aa326ab7d1df4b36982c405db44a50932f20
sourceTree <sourcetree@mail.com>
2024-01-03 19:26:24 -0700

    solving type cast problem

--- Hello.java
+++ Hello.java
@@ -2,20 +2,19 @@ import java.util.ArrayList;
 import java.util.Date;
 
 /*
- * Before generic comes, ArrayList accept to add
- * all type of object. When we call with get method
- * and assign with some type, it need to type casting
- * with that assignment type. The problem comes when we
- * cast incorrect type.
+ * Adding a generic type of String into ArrayList will give
+ * compiler to ensure the type of element we add is type of String.
+ * No need to type casting when we get element because compiler know what
+ * type of element will be called.
  */
 class Hello{
     public static void main(String[] args) {
-        ArrayList names = new ArrayList();
-        names.add(new Date());
+        ArrayList<String> names = new ArrayList<String>();
+        //names.add(new Date());    //cannot add Date type
         names.add("name 1");
         names.add("name 2");
 
-        String result = (String) names.get(0);  //incorrect type casting
+        String result = names.get(0);
         System.out.println(result);
     }
 }
